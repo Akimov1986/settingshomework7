4243ee309c7110293992a6e0b2e38b0fc7c83e12
Akimov <akimov1986@internet.ru>
2021-08-19 21:45:47 +0700

    Создал боковое меню. Но не могу сделать sid_nav_bar, gradient.

--- app/src/main/java/com/example/settings/MainActivity.java
+++ app/src/main/java/com/example/settings/MainActivity.java
@@ -1,13 +1,173 @@
 package com.example.settings;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        readSettings();
+        initToolbar();
+        initDrawer();
     }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    private void initDrawer() {
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        //ActionBarDrawerToggle
+
+        NavigationView navigationView= findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_main:
+                        showFragment(MainFragment.newInstance());
+                        break;
+                    case R.id.action_favorites:
+                        showFragment(FavoriteFragment.newInstance());
+                        break;
+                    case R.id.action_settings:
+                        showFragment(SettingsFragment.newInstance());
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_main:
+                showFragment(MainFragment.newInstance());
+                break;
+            case R.id.action_favorites:
+                showFragment(FavoriteFragment.newInstance());
+                break;
+            case R.id.action_settings:
+                showFragment(SettingsFragment.newInstance());
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void readSettings() {
+        SharedPreferences sharedPreferences = getSharedPreferences(Settings.SHARED_PR_FILE_NAME, Context.MODE_PRIVATE);
+        Settings.isDeleteFragment = sharedPreferences.getBoolean(Settings.IS_DELETE_FRAGMENT_BEFORE_ADD, false);
+        Settings.isBackIsRemove = sharedPreferences.getBoolean(Settings.IS_BACK_IS_REMOVE_FRAGMENT, false);
+        Settings.isBackStack = sharedPreferences.getBoolean(Settings.IS_BACK_STACK_USED, false);
+        Settings.isReplaceFragment = sharedPreferences.getBoolean(Settings.IS_REPLACE_FRAGMENT_USED, false);
+        Settings.isAddFragment = sharedPreferences.getBoolean(Settings.IS_ADD_FRAGMENT_USED, false);
+    }
+
+    private void initView() {
+        Button buttonBack = findViewById(R.id.btnBack);
+        Button buttonMain = findViewById(R.id.btnMain);
+        Button buttonFavorite = findViewById(R.id.btnFavorite);
+        Button buttonSettings = findViewById(R.id.btnSettings);
+
+        buttonBack.setOnClickListener(this);
+        buttonMain.setOnClickListener(this);
+        buttonFavorite.setOnClickListener(this);
+        buttonSettings.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnBack:
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                if(fragmentManager.getFragments().size()<=1) break;
+                if (Settings.isBackIsRemove) {
+                    Fragment fragmentForDelete = getVisibleFragment(fragmentManager);
+                    if (fragmentForDelete != null) {
+                        fragmentManager.beginTransaction().remove(fragmentForDelete).commit();
+                    }
+                } else {
+                    fragmentManager.popBackStack();
+                }
+
+                break;
+            case R.id.btnMain:
+                showFragment(MainFragment.newInstance());
+                break;
+            case R.id.btnFavorite:
+                showFragment(FavoriteFragment.newInstance());
+                break;
+            case R.id.btnSettings:
+                showFragment(SettingsFragment.newInstance());
+                break;
+        }
+
+    }
+
+    Fragment getVisibleFragment(FragmentManager fragmentManager) {
+        List<Fragment> flist = fragmentManager.getFragments();
+        for (int i = 0; i < flist.size(); i++) {
+            Fragment fragment = flist.get(i);
+            if (fragment.isVisible()) {
+                return fragment;
+            }
+        }
+        return null;
+    }
+
+    public void showFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction(); //создаем транзакцию
+        if (Settings.isDeleteFragment) {
+            Fragment fragmentForDelete = getVisibleFragment(fragmentManager);
+            if (fragmentForDelete != null) {
+                fragmentTransaction.remove(fragmentForDelete);
+            }
+        }
+        if (Settings.isAddFragment) {
+            fragmentTransaction
+                    .add(R.id.fragment_container, fragment);
+        } else if (Settings.isReplaceFragment) {
+            fragmentTransaction
+                    .replace(R.id.fragment_container, fragment);
+        }
+
+        if (Settings.isBackStack) {
+            fragmentTransaction.addToBackStack("");
+        }
+        fragmentTransaction.commit();
+    }
+
 }
---
+++ app/src/main/java/com/example/settings/MainFragment.java
@@ -0,0 +1,53 @@
+package com.example.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+    public static MainFragment newInstance(){
+        return new MainFragment();
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main, container,false);
+        ((TextView) v.findViewById(R.id.textView)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(requireActivity(),v);
+                requireActivity().getMenuInflater().inflate(R.menu.popup, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_main:
+                                ((MainActivity) requireActivity()).showFragment(MainFragment.newInstance());
+                                break;
+                            case R.id.action_favorites:
+                                ((MainActivity) requireActivity()).showFragment(FavoriteFragment.newInstance());
+                                break;
+                            case R.id.action_settings:
+                                ((MainActivity) requireActivity()).showFragment(SettingsFragment.newInstance());
+                                break;
+                        }
+                        return false;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
+        return v;
+
+    }
+
+
+}
